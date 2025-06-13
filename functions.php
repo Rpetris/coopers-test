c4e0f6ad7539fa1277f3ecd645089e73a2a06549
Ryan Petris <ryanpetris@hotmail.com>
2025-06-13 03:51:27 -0300

    Adição de animações dinâmicas

--- functions.php
+++ functions.php
@@ -174,7 +174,7 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
- * Load Jetpack compatibility file.
+ * Load Jetpack compatibility files.
  */
 if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
